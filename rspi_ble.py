31313f49a6be735e1f70189688713860aedca5e3
h-cha <cocoa0331oreo@gmail.com>
2021-12-24 02:31:14 +0900

    feat: 2device connected

--- rspi_ble.py
+++ rspi_ble.py
@@ -1,18 +1,15 @@
 import serial
 import time
 #import datetime
-import re
+#import re
+#count = 0
 
-ser = serial.Serial('COM3', 9600)
-
-
+ser0 = serial.Serial('/dev/rfcomm0')
+ser1 = serial.Serial('/dev/rfcomm1')
 
 while(True):
-	ser.write(b"1")
-	
-	time.sleep(60)
-	
-
-
-
+    ser0.write("right".encode())
+    ser1.write("left".encode())
+   # print(ser.readline())
+    time.sleep(5)
 ser.close()
