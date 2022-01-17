dd59527fe9c0b6200801b4adfeeb4e6d428e0e3e
h-cha <cocoa0331oreo@gmail.com>
2022-01-17 09:24:49 +0900

    gitignore

--- rspi_ble.py
+++ rspi_ble.py
@@ -4,12 +4,14 @@ import time
 #import re
 #count = 0
 
-ser0 = serial.Serial('/dev/rfcomm0')
-ser1 = serial.Serial('/dev/rfcomm1')
+# ser0 = serial.Serial('/dev/rfcomm0')
+# ser1 = serial.Serial('/dev/rfcomm1')
+ser0 = serial.Serial('/dev/tty.M5StackRIGHT')
 
 while(True):
-    ser0.write("right".encode())
-    ser1.write("left".encode())
+    ser0.write(b"1")
+    # ser1.write("left".encode())
    # print(ser.readline())
+    print(1)
     time.sleep(5)
 ser.close()
