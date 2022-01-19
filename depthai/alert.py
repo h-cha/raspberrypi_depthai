6d2aed285fa94154b1af5ae92c6e93a7cef16120
h-cha <cocoa0331oreo@gmail.com>
2022-01-19 12:59:51 +0900

    修正danger zone

--- depthai/alert.py
+++ depthai/alert.py
@@ -18,15 +18,18 @@ ser0 = serial.Serial('/dev/rfcomm0')
 class AlertM5Stack:
     def __init__(self):
         self.ONOFF = False
+        self.count = 0
 
     def parse_frame(self, results):
         
         for i, tracking in enumerate(results):
-            if tracking['now_distance'] > 0 and tracking['status'] != 'LOST':
+            # if tracking['now_distance'] > 0 and tracking['status'] != 'LOST' and self.count % 2 == 0:
+            if tracking['now_distance'] > 0 and tracking['status'] != 'LOST':    
                 self.ONOFF = True
                 ser0.write(b"2")
                 print(tracking['now_distance'])
                 print("dangerOn")
+                self.count += 1
             elif tracking['dangerous'] == True and tracking['status'] != 'LOST':
             # if tracking['dangerous'] == True and tracking['status'] != 'LOST':
                 if self.ONOFF == False:
